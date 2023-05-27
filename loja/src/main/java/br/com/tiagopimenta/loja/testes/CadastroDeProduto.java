a0ae1fe2e62a7a448ab29cda33ba10ab0c03d3c7
tiago-pimenta <tiagopimenta@gmail.com>
2023-05-27 17:10:43 -0300

    blah

--- loja/src/main/java/br/com/tiagopimenta/loja/testes/CadastroDeProduto.java
+++ loja/src/main/java/br/com/tiagopimenta/loja/testes/CadastroDeProduto.java
@@ -16,20 +16,24 @@ public class CadastroDeProduto {
 		
 		Categoria celulares = new Categoria("CELULARES");
 		
-		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares );
-		
 		EntityManager em = JPAUtil.getEntityManager();
-		
-		ProdutoDao produtoDao = new ProdutoDao(em);
-		CategoriaDao categoriaDao = new CategoriaDao(em);
 		
 		em.getTransaction().begin();
 		
-		categoriaDao.cadastrar(celulares);
-		produtoDao.cadastrar(celular);
+		em.persist(celulares);
+		celulares.setNome("XPTO");
 		
-		em.getTransaction().commit();
-		em.close();
+		em.flush();
+		em.clear();
+		
+		celulares = em.merge(celulares);
+		celulares.setNome("1234");
+		
+		em.flush();
+		em.clear();
+		
+		em.remove(celulares);
+		em.flush();
 	}
 
 }
